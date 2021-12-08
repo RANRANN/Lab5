bc8e6201f9747a27f1adb830b62bc9b02f9f768b
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2021-12-09 02:06:13 +0300

    добавил решение задачи 5.1

--- Lab5.1/Lab5.1.cs
+++ Lab5.1/Lab5.1.cs
@@ -10,6 +10,23 @@ namespace Lab5._1
     {
         static void Main(string[] args)
         {
+            int[] array = new int[7];
+            for (int i=0;i<7; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            double S = 0;
+            for (int i = 0; i < 7; i++)
+            {
+                
+                S += array[i];
+                Console.Write("{0,3 }", array[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine(S / 7);
+            Console.ReadKey();
+
+
         }
     }
 }
---
+++ Lab5.5/Lab5.5.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение N ");
+            int n = Convert.ToInt32(Console.Read());
+            int[,] array = new int[n, n];
+            int i = 0;
+            int j = 0;
+
+            Console.WriteLine("{0,2}",array[i,j]);
+            Console.ReadKey();
+        }
+    }
+}
