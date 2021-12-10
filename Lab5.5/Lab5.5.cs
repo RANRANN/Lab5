c734dab56cb05a31a9f98e583495ce4a10765f50
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2021-12-11 00:31:47 +0300

    исправил задачу 5,5

--- Lab5.5/Lab5.5.cs
+++ Lab5.5/Lab5.5.cs
@@ -13,14 +13,18 @@ namespace Lab5._5
             Console.Write("Введите значение N = ");
             int n = Convert.ToInt32(Console.ReadLine());
             int[,] array = new int[n, n];
-            int k = 0;
-            /*int i = 0;
-            int j = 0;*/
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
-                    k = (k(Math.IEEERemainder(i + j, 2)) = 0) ? Console.Write("{0}", 1) : Console.Write("{0}", 0);
+                    if ((i+j)%2==0)
+                    {
+                        Console.Write("{0}", 1);
+                    }
+                    else
+                    {
+                        Console.Write("{0}", 0);
+                    }
                 }
                 Console.WriteLine();
             }
