9ef0909c258d027df66cfb30485ad2257ad92a17
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2021-12-10 21:34:46 +0300

    добавил решение 5.5

--- Lab5.5/Lab5.5.cs
+++ Lab5.5/Lab5.5.cs
@@ -10,13 +10,20 @@ namespace Lab5._5
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите значение N ");
-            int n = Convert.ToInt32(Console.Read());
+            Console.Write("Введите значение N = ");
+            int n = Convert.ToInt32(Console.ReadLine());
             int[,] array = new int[n, n];
-            int i = 0;
-            int j = 0;
-
-            Console.WriteLine("{0,2}",array[i,j]);
+            int k = 0;
+            /*int i = 0;
+            int j = 0;*/
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    k = (k(Math.IEEERemainder(i + j, 2)) = 0) ? Console.Write("{0}", 1) : Console.Write("{0}", 0);
+                }
+                Console.WriteLine();
+            }
             Console.ReadKey();
         }
     }
